e23eb145108b7cecd7cb325e0d3ba89d53dd553d
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2023-01-25 02:33:12 +0200

    refactor database.rs

--- src-tauri/src/database.rs
+++ src-tauri/src/database.rs
@@ -1,53 +1,47 @@
 use mysql::*;
-use serde::{Serialize, Deserialize};
 use std::result;
-use std::sync::Mutex;
-use tauri::State;
-use mysql::prelude::Queryable;
+use std::sync::{Arc, Mutex};
+use lazy_static::lazy_static;
+use std::future::Future;
+use std::pin::Pin;
 
-static mut DB : Option<PooledConn> = None;
+struct DB {
+    pool: Option<PooledConn>,
+}
 
-// struct DB (Mutex<PooledConn>);
+impl DB {
+    pub fn new() -> DB {
+        DB {
+            pool: None,
+        }
+    }
 
-#[derive(Serialize, Deserialize)]
-pub struct DBResult {
-    success: bool,
-    message: String
+    pub async fn login(&mut self, host: &str, username: &str, password: &str, port: &str) -> result::Result<String, String> {
+        let url: &str = &format!("mysql://{username}:{password}@{host}:{port}");
+
+        let mut connection = || -> Result<()> {
+            let pool = Pool::new(url)?;
+            let conn = pool.get_conn()?;
+            self.pool = Some(conn);
+
+            return Ok(());
+        };
+
+        connection().map_err(|e| e.to_string())?;
+
+        Ok("Connected to database".to_string())
+    }
+}
+
+lazy_static! {
+    static ref DB_INSTANCE: Arc<Mutex<DB>> = Arc::new(Mutex::new(DB::new()));
 }
 
 #[tauri::command]
 pub async fn login(host: &str, username: &str, password: &str, port: &str) -> result::Result<String, String> {
-    let url: &str = &format!("mysql://{username}:{password}@{host}:{port}");
+    let db = DB_INSTANCE.clone();
+    let mut db = db.lock().unwrap();
+    let result = Box::new(&mut db).as_mut().login(host, username, password, port).await;
 
-    let connection = || -> Result<()> {
-        let pool = Pool::new(url)?;
-        unsafe {
-            let conn = pool.get_conn()?;
-            DB = Some(conn);
-
-            return Ok(());
-        }
-    };
-
-    connection().map_err(|e| e.to_string())?;
-
-    Ok("Connected to database".to_string())
+    result
 }
-
-// #[tauri::command]
-// pub async fn query(query: &str) -> result::Result<String, String> {
-//     unsafe {
-//         match DB {
-//             Some(conn) => {
-//                 let mut connection = &conn;
-//                 let result: Result<Vec<Row>> = (*connection).query(query);
-//                 println!("{:?}", result);
-//                 return Ok("Query executed".to_string());
-//             },
-//             None => {
-//                 println!("No connection");
-//                 return Err("No connection".to_string());
-//             }
-//         }
-//     }
-// }
